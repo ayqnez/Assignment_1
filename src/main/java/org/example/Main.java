a9e410064e93c4b0b30fec82343fd2036feaf023
ayqnez <www.ayanpro@gmail.com>
2025-02-13 17:17:36 +0500

    Add first bean implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
+        Cat cat = context.getBean(Cat.class);
+        System.out.println(cat.getName());
     }
 }
---
+++ src/main/java/org/example/ProjectConfig.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan(basePackages = "org.example")
+public class ProjectConfig {
+
+    @Bean
+    @Primary
+    public Cat cat() {
+        Cat c = new Cat();
+        c.setName("Vasya");
+        return c;
+    }
+
+    @Bean
+    public Cat catToma() {
+        Cat c = new Cat();
+        c.setName("Toma");
+        return c;
+    }
+}
